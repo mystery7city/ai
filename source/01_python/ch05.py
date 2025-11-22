186c32149107d8d6cdff89f046145375da0e8f6a
지훈 김 <darksaturday@naver.com>
2025-11-22 22:17:22 +0900

    initial

---
+++ source/01_python/ch05.py
@@ -0,0 +1,19 @@
+# ch05.py (ch05 모듈) 자동완성핫키 : ctrl+space
+<<<<<<< HEAD
+# ctrl + shift + p => select interperter를 이용해서 인터프리터(base) 선택
+# 실행 : cmd 터미널(ctrl + j)에서 python ch05.py
+def my_hello(cnt):# cnt번 반복
+    print(__name__)
+    for i in range(cnt):
+        print('Hello, python', end = '\t')
+=======
+# ctrl+ shift + p => select interpreter을 이용해서 인터프리터(base) 선택
+# 실행 : cmd 터미널(ctrl+j)에서 python ch05.py
+def my_hello(cnt): #cnt번 반복 
+    print(__name__)
+    for i in range(cnt):
+        print('Hello, Python', end='\t')
+>>>>>>> 40efe81c1a79bf543f4ff28ca8d717304047cbc1
+        print('Hello, World')
+if __name__ == '__main__':
+    my_hello(2)
---
+++ source/03_JavaScript/ch02_기본문법/6_false.js
@@ -0,0 +1,17 @@
+
+var i;
+console.log(Boolean(i));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(Number("a")));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean());
+console.log(Boolean("0==false의 결과 :", 0==false));
+console.log(Boolean("0==false의 결과 :", 0===false));
+
+
+
+
+
+
