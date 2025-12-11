db4ecacbf21981e07ca70410b290fc4249398269
mystert7city <mystery7city@naver.com>
2025-12-11 17:55:25 +0900

    12-11

--- source/08_LLM/3_summaryWeb.py
+++ source/08_LLM/3_summaryWeb.py
@@ -1,8 +1,9 @@
-# ctrl+shift + p => python:select interpreter 선택 => llm 가상환경 선택
-# 실행 : ctrl+j
-# 내일 -> 웹 버전 (입력된 데이터를 1줄 요약해서 보여주기)
-from dotenv import load_dotenv
+# 인터프리터선택(ctrl + shift + p) -> streamlit run 3_summaryWeb.py
+import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
 def askGPT(prompt):
     "GPT에게 요청하여 prompt내용을 1줄 요약하여 return"
     load_dotenv()
@@ -19,12 +20,18 @@ def askGPT(prompt):
     )
     return response.choices[0].message.content
 
-if __name__=="__main__":
-    message = input("요약할 글을 입력하세요")
-    if message:
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="요약 프로그램")
+    # st.text(os.getenv('OPENAI_API_KEY'))
+    st.header("요약 프로그램")
+    st.markdown("---")
+    message = st.text_area("요약할 글을 입력하세요")
+    if st.button("요약"):
         prompt = f"""your task is to summarize the text sentences in Korean language.
         Summarize in 1 line. Use the format of a bullet point.
         text : {message}"""
-        result = askGPT(prompt)
-        print("~ ~ ~ 요약입니다 ~ ~ ~")
-        print(result)
+        st.info(askGPT(prompt=prompt))
+
+if __name__ == "__main__":
+    main()
