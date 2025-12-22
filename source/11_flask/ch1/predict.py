0337fe4daf2766bb7fe9d00b6b21ff3b51547c9a
mystert7city <mystery7city@naver.com>
2025-12-22 11:51:41 +0900

    12-22

---
+++ source/11_flask/ch1/predict.py
@@ -0,0 +1,11 @@
+# predict.py 파일
+# 가상환경만들기 : python -m venv .venv : .venv가상환경 생성
+# 가상환경 들어가기 : .venv\Scripts\activate
+# pip업그레이드 : python -m pip install --upgrade pip
+# pip install statsmodels joblib
+# pip install xlwings
+# pip install flask
+
+
+# pip freeze > requirements.txt
+# requirements.txt xladnjsRlfl rhddb
