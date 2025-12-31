472f494387f2e639410f3ae471031830228a98d6
mystert7city <mystery7city@naver.com>
2025-12-31 17:45:10 +0900

    12-31*****

--- source/11_flask/ch6_todo/database/connection.py
+++ source/11_flask/ch6_todo/database/connection.py
@@ -1,6 +1,6 @@
 # pip install python-dotenv
 # pip freeze > requirements.txt
-import oracledb
+import cx_Oracle
 import os
 from dotenv import load_dotenv
 # conn = cx_Oracle.connect("scott", "tiger", "127.0.0.1:1521/xe")
@@ -9,6 +9,6 @@ dbserver_ip = os.getenv('DBSERVER_IP')
 oracle_port = os.getenv('ORACLE_PORT')
 oracle_user = os.getenv('ORACLE_USER')
 oracle_password = os.getenv('ORACLE_PASSWORD')
-conn = oracledb.connect(oracle_user, 
-                oracle_password,
-                f"{dbserver_ip}:{oracle_port}/xe")
+conn = cx_Oracle.connect(oracle_user, 
+                  oracle_password,
+                  f"{dbserver_ip}:{oracle_port}/xe")
